2480050a44c524f1007184d4ffc5306a97c5d9e5
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2024-04-17 00:14:14 +0300

    change name of instance

--- instance.tf
+++ instance.tf
@@ -16,7 +16,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "3.0.0"
 
-  name                    = "oleh-kurpiak-lab-05"
+  name                    = "oleh-kurpiak-lab-05-name-update"
   ami                     = "ami-0b9932f4918a00c4f"
   instance_type           = "t2.micro"
   key_name                = "oleh_kurpiak"
